2e23f0519513d5619ea14a02573e5ea16b527b37
Dhruv Patel <dhruvpatel@Dhruvs-MacBook-Air.local>
2023-11-09 11:13:58 -0500

    Started codding

---
+++ Account.py
@@ -0,0 +1,9 @@
+class Account:
+    def __init__(self , account_number , account_Holder_Name , Rate_of_Interest , current_Balance):
+
+        self.account_number = account_number
+        self.account_Holder_Name = account_Holder_Name
+        self.Rate_of_Interest = Rate_of_Interest
+        self.current_Balance = current_Balance
+
+        
--- App.py
+++ App.py
@@ -1,4 +1,3 @@
-class Account:
-    def __init__(self , account_number , account_Holder_Name , Rate_of_Interest , current_Balance):
 
-        self.account_
+
+        
