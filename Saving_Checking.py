cc9ef5496ba831b34f3507cebe3feceee486b6a5
Dhruv Patel <dhruvpatel@Dhruvs-MacBook-Air.local>
2023-11-09 13:22:19 -0500

    Error correction

--- Saving_Checking.py
+++ Saving_Checking.py
@@ -11,12 +11,12 @@ class Saving_Account(Account):
 
         Interest_Amount = self.current_Balance * self.interset_rate
         self.current_Balance += Interest_Amount
-        
+
         return f"Interest applied amount : {Interest_Amount}. New Balance is {self.current_Balance}"
 
 class CheckingAccount(Account):
 
-    def __init__(self ,account_number , account_Holder_Name , overdraft_limit , current_Balance):        
+    def __init__(self ,account_number , account_Holder_Name , overdraft_limit  , current_Balance = 0):        
 
         super().__init__(account_number , account_Holder_Name , current_Balance)
         self.overdraft_limit = overdraft_limit
