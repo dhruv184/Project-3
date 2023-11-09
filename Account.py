ded7b50cd4c56143a6bcfabf5db3015f4a3dd39e
Dhruv Patel <dhruvpatel@Dhruvs-MacBook-Air.local>
2023-11-09 11:27:14 -0500

    Added code of Account

--- Account.py
+++ Account.py
@@ -1,9 +1,35 @@
 class Account:
-    def __init__(self , account_number , account_Holder_Name , Rate_of_Interest , current_Balance):
+
+    def __init__(self , account_number , account_Holder_Name , current_Balance):
 
         self.account_number = account_number
         self.account_Holder_Name = account_Holder_Name
-        self.Rate_of_Interest = Rate_of_Interest
         self.current_Balance = current_Balance
 
+    def get_Account_Number(self):
+
+        return f"Account Number is {self.account_number}"    
+    
+    def get_Account_Holder_Name(self):
+
+        return f"Account Holder Name is {self.account_Holder_Name}"
+    
+    def get_Balance(self):
+
+        return f"Current Balance is {self.current_Balance}"
+    
+    def Deposit( self , amount) :
+
+        self.current_Balance +=amount
+        return f"Deposited amount : {amount}. New Balance is {self.current_Balance}"
+    
+    def Withdraw( self , amount ): 
+
+        if self.current_Balance >= amount:
+
+            self.current_Balance -= amount
+            return f"Withdrawal amount : {amount}. New Balance is {self.current_Balance}"
         
+        else:
+
+            return "Insufficient Funds"
